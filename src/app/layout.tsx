c772c29e0e4fd3256d5f56b74c3b25f5d8677507
InaaraKalani <kalani.inaara@gmail.com>
2025-07-16 19:14:07 +0500

    Created About Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         >
           <div className="flex flex-col min-h-screen bg-gradient-to-br from-background via-background to-muted/20 scrollbar-thin">
             <Header />
-            {children}
+            <div className="flex-1">{children}</div>
             <Footer />
           </div>
         </ThemeProvider>
